6ef4f2e8fd3cefe53e6fbff73ce2d215dd35b516
iamrajajamal <martian.hacked@gmail.com>
2019-01-05 20:11:56 +0500

    register method to the Auth service

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -6,12 +6,12 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
-  baseurl = 'http://localhost:5000/api/auth/';
+  baseUrl = 'http://localhost:5000/api/auth/';
 
   constructor(private http: HttpClient) {}
 
   login(model: any) {
-    return this.http.post(this.baseurl + 'login', model).pipe(
+    return this.http.post(this.baseUrl + 'login', model).pipe(
       map((response: any) => {
         const user = response;
         if (user) {
@@ -19,5 +19,9 @@ export class AuthService {
         }
       })
     );
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
   }
 }
