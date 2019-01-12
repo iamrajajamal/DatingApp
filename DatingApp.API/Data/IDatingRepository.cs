82877e7a1452e1752a3babef359e440668bcba04
iamrajajamal <martian.hacked@gmail.com>
2019-01-12 17:05:01 +0500

    add dating repository

---
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    interface IDatingRepository
+    {
+        // add method for both user and photos generic method
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveAll();
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUser(int id);
+
+    }
+}
