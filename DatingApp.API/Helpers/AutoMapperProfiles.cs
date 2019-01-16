e98c6d898a65f1f621e755c87cc140a261ceec83
iamrajajamal <martian.hacked@gmail.com>
2019-01-16 18:46:48 +0500

    add automapper package and implement dependency injection in userController

---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>();
+            CreateMap<User, UserForDetailedDto>();
+        }
+    }
+}
