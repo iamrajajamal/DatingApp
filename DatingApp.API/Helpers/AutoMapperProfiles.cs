ddd8e5c87c146d0c15bf0039686f5bcf06617d58
iamrajajamal <martian.hacked@gmail.com>
2019-01-19 20:17:45 +0500

    added userupdatedto and added updateuser in usercontroller

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -31,6 +31,8 @@ namespace DatingApp.API.Helpers
                     opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
                 });
             CreateMap<Photo, PhotosForDetailedDto>();
+
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
