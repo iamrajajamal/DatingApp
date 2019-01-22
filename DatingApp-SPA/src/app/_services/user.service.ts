632b8ed1ac9f997852660835c517253d2f9ced49
iamrajajamal <martian.hacked@gmail.com>
2019-01-22 19:39:39 +0500

    adding photo functionality in angular

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -25,4 +25,8 @@ constructor(private http: HttpClient) { }
     return this.http.put(this.baseUrl + 'users/' + id, user);
   }
 
+  setMainPhoto(userId: number, id: number) {
+    return this.http.post(this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain', {});
+  }
+
 }
