9b1ffc9b027a128ab3856f72c4212ab1242d9781
iamrajajamal <martian.hacked@gmail.com>
2019-02-11 11:39:42 +0500

    designing the inbox and outbox

--- DatingApp-SPA/src/app/_resolvers/messages.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/messages.resolver.ts
@@ -20,7 +20,7 @@ export class MessagesResolver implements Resolve<Message[]> {
   ) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<Message[]> {
-    return this.userService.getUsers(this.authService.decodedToken.nameid,
+    return this.userService.getMessages(this.authService.decodedToken.nameid,
       this.pageNumber, this.pageSize, this.messageContainer).pipe(
       catchError(error => {
         this.alertify.error('Problem retrieving messages');
